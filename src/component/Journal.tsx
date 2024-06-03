ad730c2eb1ffbe765eb17116bedd9a2fbb54691c
Younghyun <yhbae97@gmail.com>
2024-06-03 17:20:20 +0200

    migrate Journal.jsx into TypeScript

---
+++ src/component/Journal.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Journal: React.FC = () => {
+  return (
+    <>
+      Journal UI will be here
+      <button>
+        submit
+      </button>
+    </>
+  );
+}
+
+export default Journal;
